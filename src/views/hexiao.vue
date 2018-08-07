2a6bb5a54fb0abcbd1f410f17e68bee72ead81c0
chounan <chounannan@juniuo.com>
2018-08-07 09:50:28 +0800

    debug

--- src/views/hexiao.vue
+++ src/views/hexiao.vue
@@ -74,7 +74,7 @@
                     this.$ajax.get("merchant/order/koubei/ticket.json", {
                         params: {
                             shopId: self.shopId,
-                            ticketNo: Number(self.receiptCode),
+                            ticketNo: self.receiptCode,
                             isQuery: 'T'
                         }
                     }).then(function (result) {
@@ -90,7 +90,7 @@
                     this.$ajax.get("xmd/tuangou/receipt/prepare.json",{
                         params: {
                             storeId: self.shopId,
-                            receiptCode: Number(self.receiptCode)
+                            receiptCode: self.receiptCode
                         }
                     }).then(function (result) {
                         console.log(result.data);
