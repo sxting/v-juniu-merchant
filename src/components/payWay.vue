3d38c4c18bff35e217a6f64b94c91439a9d20c54
cuiwang <417787204@qq.com>
2018-09-29 10:40:02 +0800

    add

--- src/components/payWay.vue
+++ src/components/payWay.vue
@@ -4,19 +4,19 @@
       <div class="content">
         <div class="title">扫码收款</div>
         <div class="ub ub-ac ub-pj mt05">
-          <div class="item" @click="focusclick">
+          <div class="item" @click="submit">
             <img src="../assets/ion_sys.png" class="udb">
             <p class="sc f12">扫一扫</p>
           </div>
-          <div class="" >
+          <div class="" v-if="false">
             <!-- <img src="../assets/ion_alipay.png" class="udb">
             <p class="sc f12">支付宝</p>           -->
             <input type="number" class="inputvalues" @blur="focusState = false" v-focus="focusState" :value="inputvalue" @change="inputvalueFun($event.target.value)">
           </div>
-          <div class="item" >
+          <div class="item" v-if="false">
             <button class="btn_common" style="width: 1.1rem;
     height: 0.6rem;
-    line-height: 0.6rem;"  @click="toerwmCharge('saomai',inputvalue)">确认</button>
+    line-height: 0.6rem;" @click="toerwmCharge('saomai',inputvalue)">确认</button>
             <!-- <img src="../assets/ion_wx.png" class="udb">
             <p class="sc f12">微信</p>           -->
           </div>
@@ -28,7 +28,7 @@
         <div class="title">记录收款</div>
         <div class="ub ub-ac ub-pj mt05">
           <div class="item">
-            <img src="../assets/ion_alipay.png"  @click="toerwmCharge('ALIPAY')" class="udb">
+            <img src="../assets/ion_alipay.png" @click="toerwmCharge('ALIPAY')" class="udb">
             <p class="sc f12">支付宝</p>
           </div>
           <div class="item">
@@ -36,11 +36,11 @@
             <p class="sc f12">微信</p>
           </div>
           <div class="item">
-            <img src="../assets/ion_cash.png"  @click="toerwmCharge('CASH')" class="udb">
+            <img src="../assets/ion_cash.png" @click="toerwmCharge('CASH')" class="udb">
             <p class="sc f12">现金</p>
           </div>
           <div class="item">
-            <img src="../assets/ion_bank.png"  @click="toerwmCharge('BANKCARD')" class="udb">
+            <img src="../assets/ion_bank.png" @click="toerwmCharge('BANKCARD')" class="udb">
             <p class="sc f12">银行卡</p>
           </div>
         </div>
@@ -51,7 +51,7 @@
 </template>
 <script>
 export default {
-  props: ["isShowPayWay",'payWay','increment'],
+  props: ["isShowPayWay", "payWay", "increment"],
   data() {
     return {
       option: {
@@ -59,38 +59,56 @@ export default {
         title: "提示",
         content: "请确认"
       },
-      inputvalue:'',
-      focusState:false
+      inputvalue: "",
+      focusState: false
     };
   },
   directives: {
     focus: {
-      update: function (el, {value}) {
+      update: function(el, { value }) {
         if (value) {
-          el.focus()
+          el.focus();
         }
       }
     }
   },
   methods: {
-    focusclick () {
-      this.focusState = true
+    focusclick() {
+      this.focusState = true;
     },
-    inputvalueFun(event){
+    submit: function() {
+      let that = this;
+      // 调起微信扫一扫
+      wx.scanQRCode({
+        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果
+        scanType: [ "barCode"], // 配置扫描二维码和条形码
+        success: function(res) {
+          // 扫描的结果
+          var result = res.resultStr;
+          // alert(result.split(",")[1]);
+          that.toerwmCharge('saomai',result.split(",")[1])
+        },
+        fail: function(res) {
+          console.log(res);
+          alert(JSON.stringify(res));
+        }
+      });
+    },
+    inputvalueFun(event) {
       this.inputvalue = event;
     },
-    toerwmCharge(str,value) {
+    toerwmCharge(str, value) {
       // this.$emit("update:payWay", str);
-      let st = str
-      if(st!=='saomai'&&!value) this.$emit("increment", st);
-      else{
-        if(value&&value.length>=18){
-          this.$emit("increment", st,value);
-        }else{
-          alert('条形码格式不正确')
+      let st = str;
+      if (st !== "saomai" && !value) this.$emit("increment", st);
+      else {
+        if (value && value.length >= 18) {
+          this.$emit("increment", st, value);
+        } else {
+          alert("条形码格式不正确");
         }
       }
-      
+
       // this.$store.commit("updatePayType", str);
       // this.$router.push("/erwmCharge");
     },
@@ -102,6 +120,30 @@ export default {
     close() {
       this.$emit("update:isShowPayWay", false);
     }
+  },
+  created() {
+    // 所有需要使用JS-SDK的页面必须先注入配置信息，否则将无法调用，同一个url仅需调用一次
+    // 通过后台接口获得配置
+    this.$ajax
+      .get("https://w.juniuo.com/merchant/get_js_api_config.json", {
+        params: {
+          debug: false,
+          jsApiList: "scanQRCode", // 需要的jsapi权限，多个用半角逗号分隔
+          url: location.href.split("#")[0] // 获取到当前url
+        }
+      })
+      .then(function(response) {
+        var resp = response.data;
+        if (resp.success) {
+          // 注入配置
+          wx.config(resp.data);
+        } else {
+          alert("config error:" + resp.errorInfo);
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 };
 </script>
@@ -117,13 +159,13 @@ export default {
   background: rgba(0, 0, 0, 0.5);
   z-index: 999;
 }
-.inputvalues{
-      width: 3rem;
-    border: 1px solid #e5e5e5;
-    /* background-color: white; */
-    height:0.6rem;
-    border-radius: 0.1rem;
-    padding: 0.1rem;
+.inputvalues {
+  width: 3rem;
+  border: 1px solid #e5e5e5;
+  /* background-color: white; */
+  height: 0.6rem;
+  border-radius: 0.1rem;
+  padding: 0.1rem;
 }
 .paybox {
   width: 100%;
